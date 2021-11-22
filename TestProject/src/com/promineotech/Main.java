c28e6142c6b4dde4f72ee31bf5ab86f2e5e00174
Allen Bellinger <bellingerallen@gmail.com>
2021-11-22 15:03:53 -0600

    Practice commit

---
+++ TestProject/src/com/promineotech/Main.java
@@ -0,0 +1,13 @@
+package com.promineotech;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is only a test");
+        int result;
+        int number1 = 2;
+        int number2 = 3;
+        result = number1 + number2;
+        System.out.println("Result: " + result);
+    }
+}
